85eacd247181ee5606e909699b940db0d361dcb3
Ziyad <ziyadguliyeev@gmail.com>
2020-01-30 00:20:40 +0000

    initial commit

---
+++ uilities/database_uilities.py
@@ -0,0 +1,6 @@
+def create_database(database_path: str):
+    #todo: enerae the daabase
+    pass
+def save_words_to_database(database_path:str, words_list:list):
+    #todo: analyze the text
+    pass
---
+++ uilities/url_uilities.py
@@ -0,0 +1,9 @@
+def load_urls_from_file(file_path:str):
+    #todo: add the code needed to read the text file
+    pass
+def load_page(url: str):
+    #todo: add the code hat reads the url
+    pass
+def scrape_page(page_contents: str):
+    #todo: analyze e text
+    pass
